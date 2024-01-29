bf5023323f5700d276b427a666f16e0842793ac8
TobiLight <jgetitdonefast@gmail.com>
2024-01-29 19:13:49 +0100

    ✔ Returns a specific page of data from a dataset

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+"""
+Simple pagination module.
+"""
 import csv
 from typing import List
 from typing import Tuple
@@ -8,7 +10,7 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Returns a tuple of size two containing a start index
-    and an end index
+    and an end index.
     """
     start = (page - 1) * page_size
     end = start + page_size
